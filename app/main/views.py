483e018363f2bd8ac99b9f1d51ea47dd0d5d9811
dunyung1 <dshege4@gmail.com>
2019-02-10 21:56:43 +0300

    put a reminder for a login decorator

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 # Views
 @main.route('/')
@@ -9,3 +10,6 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+# Input! the flask_login decorator '@login_required' that will intercept a request and check if 
+# the user is authenticated and if not the user will be redirected to the login page.
